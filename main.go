4367d5128359d86432a686be232d3197ca350dcf
Vasiliy Tolstov <v.tolstov@selfip.ru>
2014-07-10 13:40:28 +0400

    fix compile
    Signed-off-by: Vasiliy Tolstov <v.tolstov@selfip.ru>

--- main.go
+++ main.go
@@ -1,6 +1,9 @@
 package main
 
-import "github.com/mitchellh/packer/packer/plugin"
+import (
+	"github.com/mitchellh/packer/packer/plugin"
+	"github.com/vtolstov/packer-post-processor-shell/shell"
+)
 
 func main() {
 	server, err := plugin.Server()
